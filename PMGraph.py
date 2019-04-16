1c2a23796146e35282ec403a3bbfc8eeb496e8b3
Raphaël <raphael.charbey@imt-atlantique.fr>
2019-04-16 15:46:26 +0200

    add utils

---
+++ PMGraph.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import csv
+
+class PMVertex(object):
+    def __init__(self, pmgraph, id_vertex, community, snapshot, nb_members):
+        self.pmgraph = pmgraph
+        self.id = id_vertex
+        self.community = community
+        self.snapshot = snapshot
+        self.nb_members = nb_members
+        
+    def get_mask_vector(self):
+        
+        in_vector = []
+        if not self.snapshot == 0:
+            for pme_in in self.pmgraph.edge_set_sequence[self.snapshot - 1]:
+                if pme_in.target == self.id:
+                    in_vector.append(pme_in.weight)
+                    
+        
+        out_vector = []
+        if not self.snapshot == len(self.pmgraph.vertex_set_sequence) - 1:
+            for pme_out in self.pmgraph.edge_set_sequence[self.snapshot]:
+                if pme_out.source == self.id:
+                    out_vector.append(pme_out.weight)
+                    
+        return (in_vector, out_vector)
+                    
+        
+                
+        
+class PMEdge(object):
+    def __init__(self, pmgraph, snapshot, pmvertex_1_id, pmvertex_2_id, weight):
+        self.pmgraph = pmgraph
+        self.source = pmvertex_1_id
+        self.target = pmvertex_2_id
+        self.snapshot_from = snapshot
+        self.snapshot_to = snapshot + 1
+        self.weight = weight
+        
+
+class PMGraph(object):
+    def __init__(self, vertex_set_sequence, edge_set_sequence):
+        self.all_vertices = []
+        self.all_edges = []
+        self.vertex_set_sequence = [{} for x in range(len(vertex_set_sequence))] 
+        self.edge_set_sequence = [[] for x in range(len(edge_set_sequence))]
+        
+        nb_vertices = 0
+        
+        for i in range(len(vertex_set_sequence)):
+            for v, nb in vertex_set_sequence[i]:
+                pmvertex = PMVertex(self, nb_vertices, v, i, nb)
+                self.all_vertices.append(pmvertex)
+                self.vertex_set_sequence[i][v] = pmvertex
+                nb_vertices += 1
+                
+        for i in range(len(edge_set_sequence)):
+            for vertex_1, vertex_2, w in edge_set_sequence[i]:
+                pmv_1_id = self.vertex_set_sequence[i][vertex_1]
+                pmv_2_id = self.vertex_set_sequence[i+1][vertex_2]
+                pmedge = PMEdge(self, i, pmv_1_id, pmv_2_id, w)
+                self.all_edges.append(pmedge)
+                self.edge_set_sequence[i].append(pmedge)
+        
+    @classmethod
+    def read(cls, filename):
+        
+        _max_snapshot = -1
+        _vertex_set_sequence = []
+        with open(filename + '_nb_members_per_community.csv', 'r') as to_read:
+            csvr = csv.reader(to_read)
+            next(csvr)
+            for line in csvr:
+                snapshot, community_1, nb_members = (int(x) for x in line)
+                if snapshot > _max_snapshot:
+                    _vertex_set_sequence.append(set())
+                    _max_snapshot = snapshot
+                _vertex_set_sequence[snapshot].add((community_1, nb_members))
+         
+        _edge_set_sequence = [[] for x in range(_max_snapshot)]          
+        with open(filename + '_flows.csv', 'r') as to_read:
+            csvr = csv.reader(to_read)
+            next(csvr)
+            for line in csvr:
+                snapshot, community_1, community_2, weight = (int(x) for x in line)
+                _edge_set_sequence[snapshot].append((community_1, community_2, weight))
+           
+        return cls(_vertex_set_sequence, _edge_set_sequence)
+    
+    def get_mask_vectors(self):
+        all_masks = {}
+        for pmv in self.all_vertices:
+            all_masks[pmv.id] = pmv.get_mask_vector()
+    
+        
+        
+PMGraph.read('../Data/edgescommunities').get_mask_vectors()
+        
+            
+            
--- utils.py
+++ utils.py
@@ -10,18 +10,45 @@ import csv
 
 Data = '../Data/'
 
-def is_member_number(line):
-    return len(line[2][1].split('.')) == 2
+class formating_data(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.n_rows = []
+        self.flow_rows = []
 
-# From old csv format to new in two files
-def format_csv(csv_name):
-    n_rows, flow_rows = [], []
-    with open(Data + csv_name):
-        csvr = csv.reader(to_read)
-        for line in csvr:
-            s = line[1][1]
-            community_1 = line[1][0]
-            community_2 = line[2][0]
-            value = line[3]
-            if is_member_number(line):
-                n_rows.append([s, community_1, value])
+    def is_member_number(self, line):
+        return line[2].strip('()').split(',')[1][-2:] == '.1'
+    
+    # From old csv format to new in two files
+    def read_csv(self):
+        with open(Data + self.filename + '.csv') as to_read:
+            csvr = csv.reader(to_read)
+            next(csvr)
+            for line in csvr:
+                s = line[1].strip('()').split(',')[1]
+                community_1 = line[1].strip('()').split(',')[0]
+                community_2 = line[2].strip('()').split(',')[0]
+                value = int(line[3])
+                if self.is_member_number(line):
+                    self.n_rows.append([s, community_1, value])
+                else:
+                    s = s.split('.')[0]
+                    self.flow_rows.append([s, community_1, community_2, value])
+                
+    def write_data(self):
+        with open(Data + self.filename + '_nb_members_per_community.csv', 'w') as to_write:
+            csvw = csv.writer(to_write)
+            csvw.writerow(['snapshot', 'community', 'nb_members'])
+            for row in self.n_rows:
+                csvw.writerow(row)
+                
+        
+        with open(Data + self.filename + '_flows.csv', 'w') as to_write:
+            csvw = csv.writer(to_write)
+            csvw.writerow(['snapshot', 'community_1', 'community_2', 'weight'])
+            for row in self.flow_rows:
+                csvw.writerow(row)
+                
+    def run(self):
+        self.read_csv()
+        self.write_data()
